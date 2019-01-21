e8cf053edf7b7cafd68d2692f6a67e3829f98516
David Lenfesty <lenfesty@ualberta.ca>
2019-01-21 12:21:06 -0700

    Cleaned up build stuff a little bit

--- opencv_libs.cmake
+++ opencv_libs.cmake
@@ -1,10 +1,18 @@
-# File for which opencv libraries should be linked against
-# Needs to have OPENCV_INSTALL_DIR defined previously
+# Includes files and such that OpenCV requires
+
+set(OPENCV_INSTALL_DIR "/usr" CACHE STRING "Location of OpenCV installation")
+set(OPENCV_INC_DIR "include" CACHE STRING "Directory that contains OpenCV includes")
+set(OPENCV_LIB_DIR "lib" CACHE STRING "Directory that contains OpenCV libraries")
+
+include_directories("/usr/local/include")
+
+set(OPENCV_LIB_PREFIX ${OPENCV_INSTALL_DIR}/${OPENCV_LIB_DIR})
+
 list(APPEND OPENCV_LIBS
-    "${OPENCV_INSTALL_DIR}/lib/libopencv_core.so"
-    "${OPENCV_INSTALL_DIR}/lib/libopencv_highgui.so"
-    "${OPENCV_INSTALL_DIR}/lib/libopencv_imgcodecs.so"
-    "${OPENCV_INSTALL_DIR}/lib/libopencv_flann.so"
-    "${OPENCV_INSTALL_DIR}/lib/libopencv_features2d.so"
-    "${OPENCV_INSTALL_DIR}/lib/libopencv_xfeatures2d.so"
+    "${OPENCV_LIB_PREFIX}/libopencv_core.so"
+    "${OPENCV_LIB_PREFIX}/libopencv_highgui.so"
+    "${OPENCV_LIB_PREFIX}/libopencv_imgcodecs.so"
+    "${OPENCV_LIB_PREFIX}/libopencv_flann.so"
+    "${OPENCV_LIB_PREFIX}/libopencv_features2d.so"
+    "${OPENCV_LIB_PREFIX}/libopencv_xfeatures2d.so"
 )

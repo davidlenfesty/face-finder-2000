41c3a147192398e0f1ee2020206db37551ac69f7
David Lenfesty <lenfesty@ualberta.ca>
2019-01-21 12:12:30 -0700

    Now it will just find good matches on two hardcoded files

--- face_finder.cpp
+++ face_finder.cpp
@@ -22,8 +22,8 @@ int main(int argc, char** argv) {
     matcher = FlannBasedMatcher::create();
 
     // Read in images
-    Mat train_image = imread("/home/isthatme/face_train.png", CV_LOAD_IMAGE_COLOR);
-    Mat query_image = imread("/home/isthatme/face_query.png", CV_LOAD_IMAGE_COLOR);
+    Mat train_image = imread("/home/isthatme/face-finder-2000/face_train.jpg", IMREAD_COLOR);
+    Mat query_image = imread("/home/isthatme/face-finder-2000/face_query.jpg", IMREAD_COLOR);
 
     vector<KeyPoint> train_keypoints, query_keypoints;
     Mat train_descriptors, query_descriptors;
@@ -35,12 +35,20 @@ int main(int argc, char** argv) {
     vector< vector<DMatch> > matches;
     // Match descriptors within a certain distance.
     // "Distance" being a term for similarity I think
-    matcher->radiusMatch(query_descriptors, train_descriptors, matches, 5); // TODO: tweak radius
+    matcher->knnMatch(query_descriptors, train_descriptors, matches, 2); // TODO: tweak radius
+
+    // Find "good" matches. Just doing this for now to reduce to a 1d matrix
+    vector<DMatch> good_matches;
+    for (int i = 0; i < static_cast<int>(matches.size()); i++) {
+        if (matches[i][0].distance < matches[i][1].distance * 0.7) {
+            good_matches.push_back(matches[i][0]);
+        }
+    }
 
     Mat out_image;
     // Draw matches from query image to training image
     drawMatches(query_image, query_keypoints, train_image, train_keypoints,
-            matches[0], out_image, Scalar::all(-1), Scalar::all(-1),
+            good_matches, out_image, Scalar::all(-1), Scalar::all(-1),
             vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 
     imshow("Face detection", out_image);
